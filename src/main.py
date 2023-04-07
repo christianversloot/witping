5e3705192e86700d8d73fe771dec0e6adec1a414
Christian Versloot <c.versloot@infoplaza.nl>
2023-04-07 17:49:28 +0200

    Add entry point

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from mesh import Mesh
+
+def main() -> None:
+    '''
+        WitPing entry point.
+
+        Create a Mesh using provided configuration, check for Wits that fire
+        and send Pings.
+
+        Returns
+        -------
+        Void.
+    '''
+    witping_mesh = Mesh()
+
+
+if __name__ == '__main__':
+	main()
